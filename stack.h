60308b7d40b47f860f96d6f75947ea9830aa687f
Alessandro Manattini <ilmanatz97@gmail.com>
2017-05-09 00:17:35 +0200

    Mi son scordato l'include guard. Che niubbo.

--- stack.h
+++ stack.h
@@ -1,6 +1,11 @@
+#ifndef STACK_H
+#define STACK_H
+
 void init(list l);
 int push(list l, turn t);
 node pop(list l, int cell_id);
 node peek(list l, int cell_id);
 int count(list l);
 void destroy(list l);
+
+#endif
