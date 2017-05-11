0286da53be05ef845c3fc5bf7e790f8fe13222ed
Alessandro Manattini <ilmanatz97@gmail.com>
2017-05-11 15:48:17 +0200

    Non ha senso compilare un file oggetto per sole strutture dati in C. Quindi ho sostituito il file di strutture con un file di soli header.

--- structures.h
+++ structures.h
@@ -1,27 +1,28 @@
-#ifndef STRUCTURES_C
-#define STRUCTURES_C
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
 #define FALSE 0
 #define TRUE 1
+#define MAX_FIELD_LENGTH 100
 /*
  * Structure representing a cell on the field. For each cell, information
  * required are the cell id, the row number and the col number (for graphic),
  * the state (FALSE: covered, TRUE: uncovered) and the possible flag set
  * (flagged -> FALSE: not flagged, TRUE: flagged) 
  */
-struct cell{
+typedef struct Cell{
 	int id;
-	int position-x;
-	int position-y;
+	int position_x;
+	int position_y;
 	int state;
 	int flagged;
-}
+} cell;
 
 /*
  * Actually, we don't need to define a struct for the field. The state of the
  * field is contained in each of its cells, so we just define a field as a
  * matrix of cells.
  */
-typedef cell[][] field;
+typedef cell field[MAX_FIELD_LENGTH][MAX_FIELD_LENGTH];
 
 /*
  * There are several ways to implement the turn management. The implementation
@@ -30,22 +31,22 @@ typedef cell[][] field;
  * their state. For example, if an empty cell is hit then that turn will be represented
  * as a set of pushes on the array where each push is a cell id that changed its state.
  */
-struct turn{
+typedef struct Turn{
 	int turn_number;
 	int cell_id;
-}
+} turn;
 
 /*
  * The node of the linked list which will be the data structure for the stack contains,
  * of course, information of type turn.
  */
-struct node{
+typedef struct Node{
     turn info;
     struct node *ptr;
-}
+} node;
 
 /*
  * A list is nothing but a pointer to the first element.
  */
-typedef *node stack;
+typedef node* stack;
 #endif
