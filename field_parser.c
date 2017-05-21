32a7028b34f4fa3af3642333752c1b378fddc61d
Alessandro Manattini <ilmanatz97@gmail.com>
2017-05-21 15:07:14 +0200

    Corretto il bug ed aggiunto il controllo che salta le linee che inserirebbero mine fuori dal campo. Inserito in stage un esempio di utilizzo del parser. test_parse.c: file di test da compilare insieme a field_parser.c test_parse.txt : file di esempio di campo da parsare.

--- field_parser.c
+++ field_parser.c
@@ -9,7 +9,7 @@ field* parse(char* filename, int *width, int *heigth){
 	FILE *source;
 	field* parsed;
 	char *useless;
-	int *max_x, *max_y, *buff_pos_x, *buff_pos_y, id_distributor = 0, i, j;
+	int *max_x, *max_y, *buff_pos_x, *buff_pos_y, id_distributor = 0, i, j, fail;
 	parsed = (field*)malloc(sizeof(field));
 	max_x = (int*)malloc(sizeof(int));
 	max_y = (int*)malloc(sizeof(int));
@@ -19,11 +19,14 @@ field* parse(char* filename, int *width, int *heigth){
 	*buff_pos_x = *buff_pos_y = 0;
 	source = fopen(filename, "r");
 	if(!source)	return NULL;
-	/* parso prima numero di righe e di colonne */
-	fscanf(source, "%d,%d", max_x, max_y);
+	/* parso prima numero di righe e di colonne, o restituisco null se il file non è nel formato appropriato */
+	if(!fscanf(source, "%d,%d", max_x, max_y)) return NULL;
 	*width = *max_x;
 	*heigth = *max_y;
-	fscanf(source, "%s", useless);
+	/* Questa operazione solleva un warning in compilazione. E' una questione nota, ma ho bisogno di parsare 
+	 * la linea vuota dopo width e height.
+	 */
+	fscanf(source, "", useless);
 	/* inizializzo tutte le caselle con contenuto nullo */
 	for(i = 0; i < *max_x; i++)
 		for(j = 0; j < *max_y; j++){
@@ -35,13 +38,19 @@ field* parse(char* filename, int *width, int *heigth){
 		}
 	/* Qui inizia la parte veramente intelligente: per ogni riga parsata,
 	 * andiamo ad aggiungere una mina in quel punto e ad aggiornare tutti
-	 * i valori di casella attorno a quella mina
+	 * i valori di casella attorno a quella mina. Se il file non è nel formato
+	 * appropriato viene restituito NULL. Se invece una mina risulta essere
+	 * fuori dal campo semplicemente non viene inserita.
 	 */
-	while(!feof(source)){
-		fscanf(source, "%d,%d", buff_pos_x, buff_pos_y);
-		set_numbers_around(parsed, *buff_pos_x, *buff_pos_y);
-		(*parsed)[*buff_pos_x][*buff_pos_y].value = 0;
+	fail = FALSE;
+	while(!feof(source) && !fail){
+		if(!fscanf(source, "%d,%d", buff_pos_x, buff_pos_y)) fail = TRUE;
+		if(*buff_pos_x < *max_x && *buff_pos_y < *max_y){
+			set_numbers_around(parsed, *buff_pos_x, *buff_pos_y);
+			(*parsed)[*buff_pos_x][*buff_pos_y].value = 0;
+		}
 	}
+	if(fail) return NULL;
 	fclose(source);
 	return parsed;
 }
