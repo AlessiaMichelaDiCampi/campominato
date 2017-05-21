c1683ef6cf90ff67a22d3cb84222724f7e43787c
Alessandro Manattini <ilmanatz97@gmail.com>
2017-05-21 14:45:05 +0200

    Effettuate le correzioni del caso. La libreria funziona se non per un bug noto: la prima coppia del file da parsare, scritta come x,y, viene letta come y,0.

--- field_parser.c
+++ field_parser.c
@@ -2,18 +2,31 @@
 #include <stdlib.h>
 #include "structures.h"
 
-field* parse(char* filename){
+field* parse(char* filename, int *width, int *heigth);
+void set_numbers_around(field *f, int x, int y);
+
+field* parse(char* filename, int *width, int *heigth){
 	FILE *source;
 	field* parsed;
-	int max_x, max_y, buff_pos_x = 0, buff_pos_y = 0, id_distributor = 0, i, j;
+	char *useless;
+	int *max_x, *max_y, *buff_pos_x, *buff_pos_y, id_distributor = 0, i, j;
 	parsed = (field*)malloc(sizeof(field));
+	max_x = (int*)malloc(sizeof(int));
+	max_y = (int*)malloc(sizeof(int));
+	buff_pos_x = (int*)malloc(sizeof(int));
+	buff_pos_y = (int*)malloc(sizeof(int));
+	useless = (char*)malloc(sizeof(int));
+	*buff_pos_x = *buff_pos_y = 0;
 	source = fopen(filename, "r");
+	if(!source)	return NULL;
 	/* parso prima numero di righe e di colonne */
 	fscanf(source, "%d,%d", max_x, max_y);
-	fscanf(source, "%s");
+	*width = *max_x;
+	*heigth = *max_y;
+	fscanf(source, "%s", useless);
 	/* inizializzo tutte le caselle con contenuto nullo */
-	for(i = 0; i < max_x; i++)
-		for(j = 0; j < max_y; j++){
+	for(i = 0; i < *max_x; i++)
+		for(j = 0; j < *max_y; j++){
 			(*parsed)[i][j].value = -1;
 			(*parsed)[i][j].id = id_distributor;
 			(*parsed)[i][j].state = FALSE;
@@ -26,17 +39,18 @@ field* parse(char* filename){
 	 */
 	while(!feof(source)){
 		fscanf(source, "%d,%d", buff_pos_x, buff_pos_y);
-		set_numbers_around(parsed, buff_pos_x, buff_pos_y);
-		(*parsed)[buff_pos_x][buff_pos_y].value = 0;
+		set_numbers_around(parsed, *buff_pos_x, *buff_pos_y);
+		(*parsed)[*buff_pos_x][*buff_pos_y].value = 0;
 	}
 	fclose(source);
+	return parsed;
 }
-void set_numbers_around(field* f, int x, int y){
+void set_numbers_around(field *f, int x, int y){
 	int offset_x, offset_y;
 	for(offset_x = -1; offset_x <= 1; offset_x++){
 		for(offset_y = -1; offset_y <= 1; offset_y++){
-			if((*parsed)[x + offset_x][y + offset_y].value > 0) (*parsed)[x + offset_x][y + offset_y].value++;
-			else if ((*parsed)[x + offset_x][y + offset_y].value < 0) (*parsed)[x + offset_x][y + offset_y].value = 1;
+			if((*f)[x + offset_x][y + offset_y].value > 0) (*f)[x + offset_x][y + offset_y].value++;
+			else if ((*f)[x + offset_x][y + offset_y].value < 0) (*f)[x + offset_x][y + offset_y].value = 1;
 		}
 	}
 }
--- field_parser.h
+++ field_parser.h
@@ -2,7 +2,7 @@
 #define FIELD_PARSER_H
 #include "structures.h"
 
-field* parse(char* filename);
+field* parse(char* filename, int *width, int *heigth);
 void set_numbers_around(field* f, int x, int y);
 
 #endif
